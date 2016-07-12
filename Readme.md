1c02145d33bf40c75aeafa2abe2dab6b25a7d881
Henrich Kolkhorst <kolkhorst@informatik.uni-freiburg.de>
2016-07-12 18:52:12 +0200

    updated gpsd_client to work with gpsd API version 6.1 (corresponding to gpsd version 3.15-2build1 distributed in xenial apt repos)

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Fork of https://github.com/ktossell/gps_umd
+
+Added support for gpsd API version 6.1 (shipped with Ubuntu 16.04 Xenial)
--- gpsd_client/src/client.cpp
+++ gpsd_client/src/client.cpp
@@ -110,7 +110,11 @@ class GPSDClient {
 
       status.satellite_used_prn.resize(status.satellites_used);
       for (int i = 0; i < status.satellites_used; ++i) {
-        status.satellite_used_prn[i] = p->used[i];
+#if (GPSD_API_MAJOR_VERSION >= 6)
+        status.satellite_used_prn[i] = p->skyview[i].used;
+#else
+	status.satellite_used_prn[i] = p->used[i];
+#endif
       }
 
       status.satellites_visible = SATS_VISIBLE;
@@ -121,18 +125,26 @@ class GPSDClient {
       status.satellite_visible_snr.resize(status.satellites_visible);
 
       for (int i = 0; i < SATS_VISIBLE; ++i) {
-        status.satellite_visible_prn[i] = p->PRN[i];
+#if (GPSD_API_MAJOR_VERSION >= 6)
+        status.satellite_visible_prn[i] = p->skyview[i].PRN;
+        status.satellite_visible_z[i] = p->skyview[i].elevation;
+        status.satellite_visible_azimuth[i] = p->skyview[i].azimuth;
+        status.satellite_visible_snr[i] = p->skyview[i].ss;
+#else
+	status.satellite_visible_prn[i] = p->PRN[i];
         status.satellite_visible_z[i] = p->elevation[i];
         status.satellite_visible_azimuth[i] = p->azimuth[i];
         status.satellite_visible_snr[i] = p->ss[i];
+#endif
       }
-
       if ((p->status & STATUS_FIX) && !(check_fix_by_variance && isnan(p->fix.epx))) {
         status.status = 0; // FIXME: gpsmm puts its constants in the global
                            // namespace, so `GPSStatus::STATUS_FIX' is illegal.
 
+#if (GPSD_API_MAJOR_VERSION < 6)
         if (p->status & STATUS_DGPS_FIX)
           status.status |= 18; // same here
+#endif
 
         fix.time = p->fix.time;
         fix.latitude = p->fix.latitude;
@@ -194,9 +206,11 @@ class GPSDClient {
         case STATUS_FIX:
           fix->status.status = 0; // NavSatStatus::STATUS_FIX;
           break;
+#if (GPSD_API_MAJOR_VERSION < 6)
         case STATUS_DGPS_FIX:
           fix->status.status = 2; // NavSatStatus::STATUS_GBAS_FIX;
           break;
+#endif
       }
 
       fix->status.service = NavSatStatus::SERVICE_GPS;
